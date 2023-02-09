979a3c08085b268b7cfdd9aaef0e63b59d92abb1
Malvere <gatling1404@gmail.com>
2023-02-09 15:13:04 +0300

    run.py

--- run.py
+++ run.py
@@ -1,30 +1,27 @@
 # Imports
 from imports import *
 
-# Bot Init
-
-# Logging
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=env.API_TOKEN)
-dp = Dispatcher(bot)
-
-dp.middleware.setup(LoggingMiddleware())
-
-
-async def on_startup():
-    logging.warning("Starting webhook..")
-    await bot.delete_webhook()
-    await bot.set_my_commands(commands=set_cmd())
-    await bot.set_webhook(env.WEBHOOK_URL)
-
-
-async def on_shutdown():
-    logging.warning("Shutting down..")
-    logging.warning("Bye!")
-
-
 if __name__ == "__main__":
+    # Bot Init
+
+    # Logging
+    logging.basicConfig(level=logging.INFO)
+
+    bot = Bot(token=env.API_TOKEN)
+    dp = Dispatcher(bot)
+
+    dp.middleware.setup(LoggingMiddleware())
+
+    async def on_startup():
+        logging.warning("Starting webhook..")
+        await bot.delete_webhook()
+        await bot.set_my_commands(commands=set_cmd())
+        await bot.set_webhook(env.WEBHOOK_URL)
+
+    async def on_shutdown():
+        logging.warning("Shutting down..")
+        logging.warning("Bye!")
+
     commands.setup(dp)
     callback.setup(dp)
     start_webhook(
