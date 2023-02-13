1cbbad91655b07dda216b807d6db09311f2944fe
Malvere <gatling1404@gmail.com>
2023-02-13 12:34:39 +0300

    TypeError

--- handlers/commands/wgcf_tool.py
+++ handlers/commands/wgcf_tool.py
@@ -25,6 +25,6 @@ async def wgcf_tool(msg: types.Message) -> None:
                 await msg.answer("Сгенерировать QR-код?", reply_markup=kb.generate_qrcode_plus)
         except Exception as ex:
             ex_message = f"Type: {type(ex).__name__}"
-        await msg.answer(f"Ошибка генерации, попробуйте через 10 минут. \nDetails: {ex_message}")
+            await msg.answer(f"Ошибка генерации, попробуйте через 10 минут. \nDetails: {ex_message}")
     else:
         await msg.answer("No args")
