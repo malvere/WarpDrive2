5dd56ea64104495a578a26cefbcac0343d333e41
Malvere <gatling1404@gmail.com>
2023-05-25 19:23:25 +0300

    Added build file for render

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+echo "Installing the latest version of poetry..."
+
+pip install --upgrade pip
+
+pip install poetry==1.2.0
+
+rm poetry.lock
+
+poetry lock
+
+python -m poetry install
